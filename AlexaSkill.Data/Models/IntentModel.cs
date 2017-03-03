8cb5914dc3160277f417639e07668d20cbc8564d
Matt Owens <m.owens0702@gmail.com>
2017-03-03 14:04:47 -0600

    added two separate methods to handle different intents

--- AlexaSkill.Data/Models/IntentModel.cs
+++ AlexaSkill.Data/Models/IntentModel.cs
@@ -3,12 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace AlexaSkill.Data.Models
 {
+    [JsonObject]
     public class IntentModel
     {
+        [JsonProperty("name")]
         public string Name { get; set; }
-        public SlotModel Slots { get; set; }
+        [JsonProperty("slots")]
+        public dynamic Slots { get; set; }
+
+        public List<KeyValuePair<string, string>> GetSlots()
+        {
+            var output = new List<KeyValuePair<string, string>>();
+            if (Slots == null) return output;
+
+            foreach (var slot in Slots.Children())
+            {
+                if (slot.First.value != null)
+                    output.Add(new KeyValuePair<string, string>(slot.First.name.ToString(), slot.First.value.ToString()));
+            }
+
+            return output;
+        }
+
     }
 }
--- AlexaSkill/Controllers/AlexaController.cs
+++ AlexaSkill/Controllers/AlexaController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using AlexaSkill.Data;
 using AlexaSkill.Data.Models;
@@ -14,33 +15,91 @@ namespace AlexaSkill.Controllers
         [HttpPost, Route("api/alexa/demo")]
         public AlexaResponseModel Pluralsight(AlexaRequestModel alexaRequest)
         {
-            //var request = new Requests().Create(new Data.Request
-            //{
-            //    Intent = alexaRequest.Request.Intent == null ? "" : alexaRequest.Request.Intent.Name,
-            //    AppId = alexaRequest.Session.Application.ApplicationId,
-            //    RequestId = alexaRequest.Request.RequestId,
-            //    SessionId = alexaRequest.Session.SessionId,
-            //    UserId = alexaRequest.Session.User.UserId,
-            //    IsNew = alexaRequest.Session.New,
-            //    Version = alexaRequest.Version,
-            //    Type = alexaRequest.Request.Type,
-            //    DateCreated = DateTime.UtcNow
-            //});
 
-            var response = new AlexaResponseModel("Hello world. Would you like for me to say it again? Yes or no?");
+            var request = alexaRequest.Request;
+
+            AlexaResponseModel response = null;
+
+            switch (request.Type)
+            {
+                case "LaunchRequest":
+                    response = LaunchRequestHandler(request);
+                    break;
+                case "IntentRequest":
+                    response = IntentRequestHandler(request);
+                    break;
+                case "SessionEndedRequest":
+                    response = SessionEndedRequestHandler(request);
+                    break;
+            }
+
+            return response;
+        }
+
+        private AlexaResponseModel LaunchRequestHandler(RequestModel request)
+        {
+            var response = new AlexaResponseModel("Welcome to QA Resort. Do you need more towels? Or maybe you forgot a toothbrush");
             response.Response.Card.Title = "Alexo Demo";
             response.Response.Card.Content = "Hello world";
-            response.Response.Reprompt.OutputSpeech.Text = "Do you need this right away?";
+            response.Response.Reprompt.OutputSpeech.Text = "Please pick one, more towels or a toothbrush?";
             response.Response.ShouldEndSession = false;
 
-            if (alexaRequest.Request.Intent.Name == "AMAZON.NoIntent")
+            if (request.Intent.Name == "AMAZON.NoIntent")
             {
                 response.Response.OutputSpeech.Text = "Ok. When would you like them?";
                 response.Response.ShouldEndSession = true;
             }
 
+            return response;
+        }
+        private AlexaResponseModel IntentRequestHandler(RequestModel request)
+        {
+            AlexaResponseModel response = null;
+
+            switch (request.Intent.Name)
+            {
+                case "TowelsIntent":
+                    response = TowelsIntentHandler(request);
+                    break;
+                case "ToothbrushIntent":
+                    response = ToothbrushIntentHandler(request);
+                    break;
+            }
 
             return response;
+        }
+        private AlexaResponseModel SessionEndedRequestHandler(RequestModel request)
+        {
+            // return null until we have server side session variables to clear out here
+            return null;
+        }
+        private AlexaResponseModel TowelsIntentHandler(RequestModel request)
+        {
+            int numberOfTowels = 2;
+
+            if (request.Intent.GetSlots().Any())
+            {
+                var slotsList = request.Intent.GetSlots();
+                // set upper limit on number of towels
+                int maxNumberOfTowels = 10;
+                var numberOfTowelsValue = slotsList.FirstOrDefault(s => s.Key == "NumberOfTowels").Value;
+
+                if (!string.IsNullOrWhiteSpace(numberOfTowelsValue) && int.TryParse(numberOfTowelsValue, out numberOfTowels) &&
+                    !(numberOfTowels >= 1 && numberOfTowels <= maxNumberOfTowels))
+                {
+                    numberOfTowels = maxNumberOfTowels;
+                }
+            }
+
+            var output = new StringBuilder(numberOfTowels + "towels are on the way");
+
+            return new AlexaResponseModel(output.ToString());
+        }
+        private AlexaResponseModel ToothbrushIntentHandler(RequestModel request)
+        {
+            var output = new StringBuilder("Your toothbrush is on the way");
+
+            return new AlexaResponseModel(output.ToString());
         }
     }
 }
